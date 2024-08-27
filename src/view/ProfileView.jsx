79243ffb258a2f71a7340b73df816af461ad55fd
lye <fklye0912@gmail.com>
2024-08-27 16:57:51 +0900

    build: react-icon 추가

--- src/view/ProfileView.jsx
+++ src/view/ProfileView.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { CgGenderFemale } from "react-icons/cg";
+import { IoMdMusicalNote } from "react-icons/io";
 
 const ProfileContent = styled.div`
     width: 100%;
@@ -15,11 +17,16 @@ const TodayMood = styled.div`
     align-items: center;
     width: 70%;
     height: 5%;
-    border: 2px solid rgba(0, 0, 0, 0.05);
+    border: 3px solid rgba(0, 0, 0, 0.05);
+    border-radius: 5px;
     & span {
         font-size: 0.8rem;
-        font-weight: 500;
-        margin-left: 0.5rem;
+        font-weight: 600;
+        margin: 0 0.7rem 0;
+    }
+    & p {
+        margin-left: 3px;
+        font-size: 0.9rem;
     }
 `
 
@@ -28,6 +35,7 @@ const ProfileImage = styled.div`
     width: 70%;
     height: 30%;
     border: 2px solid rgba(0, 0, 0, 0.05);
+    border-radius: 5px;
 `
 
 const Comments = styled.div`
@@ -57,9 +65,24 @@ const History = styled.div`
 const Profile = styled.div`
     width: 70%;
     height: 5%;
-    & h4 {
+    & h4, div {
         color: midnightblue;
-        margin: 0.3rem 0 0;
+        margin: 0.3rem 3px 0;
+        float: left;
+    }
+    & div {
+        border: 0.5px solid rgba(0, 0, 0, 0.2);
+        border-radius: 1px;
+        box-shadow: 0px 0px 2px black;
+        height: 0.8rem;
+        margin-top: 0.7rem;
+        & svg {
+            width: 0.8rem;
+            height: 0.7rem;
+            margin: 0;
+            padding-bottom: 5px;
+            color: goldenrod;
+        }
     }
     & span {
         font-size: 0.8rem;
@@ -73,6 +96,8 @@ function ProfileView(props) {
         <ProfileContent>
             <TodayMood>
                 <span>TODAY IS ..</span>
+                <IoMdMusicalNote />
+                <p>즐거움</p>
             </TodayMood>
             <ProfileImage></ProfileImage>
             <Comments>안녕하세요.<br />추억의 싸이월드를<br />참고하여 만든<br />블로그 입니다.</Comments>
@@ -81,6 +106,7 @@ function ProfileView(props) {
             </History>
             <Profile>
                 <h4>practiceLye</h4>
+                <div><CgGenderFemale  /></div>
                 <span>fklye0912@gmail.com</span>
             </Profile>
         </ProfileContent>
